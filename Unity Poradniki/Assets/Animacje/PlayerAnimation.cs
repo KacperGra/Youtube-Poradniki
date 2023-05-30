c5c16535fbac3bca7b28721a9e7599d8a70fd5b0
KacperGra <kacperktbm@gmail.com>
2023-05-30 11:37:39 +0200

    Updated animation tutorial

--- Unity Poradniki/Assets/Animacje/PlayerAnimation.cs
+++ Unity Poradniki/Assets/Animacje/PlayerAnimation.cs
@@ -6,19 +6,31 @@ public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private PlayerMovement movement;
+    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private GroundChecker groundChecker;
     [SerializeField] private SpriteRenderer playerGraphics;
     [SerializeField] private string isMovingParameterName = "IsMoving";
+    [SerializeField] private string isGroundedParameterName = "IsGrounded";
+    [SerializeField] private string isFallingParameterName = "IsFalling";
 
     private int _isMovingHash;
+    private int _isGroundedHash;
+    private int _isFallingHash;
 
     private void Start()
     {
         _isMovingHash = Animator.StringToHash(isMovingParameterName);
+        _isGroundedHash = Animator.StringToHash(isGroundedParameterName);
+        _isFallingHash = Animator.StringToHash(isFallingParameterName);
     }
 
     private void Update()
     {
         animator.SetBool(_isMovingHash, movement.IsMoving());
+        animator.SetBool(_isGroundedHash, groundChecker.IsGrounded());
+
+        bool isFalling = rigidbody.velocity.y < -0.01f;
+        animator.SetBool(_isFallingHash, isFalling);
 
         if (movement.GetInput().x > 0)
         {
